c7e7617ce7d6887d8dc8798d89b78c4fd6199cb9
msolari <mathias.solari@fr.thalesgroup.com>
2025-10-13 18:11:52 +0200

    ajout simple_event_example

---
+++ Examples/simple_event_example/01-Components/Receiver/C/inc/Receiver_user_context.h
@@ -0,0 +1,30 @@
+/* @file Receiver_user_context.h
+ * This is an example of a user defined User Module context
+ */
+
+ #if !defined(Receiver_USER_CONTEXT_H)
+ #define Receiver_USER_CONTEXT_H
+
+ #if defined(__cplusplus)
+ extern "C" {  
+ #endif /* __cplusplus */
+
+ #include "extra_types.h"
+ #include "extra_types_types.h"
+ /* Container Types */
+ #include "Receiver_container_types.h"
+
+ /* User Module Context structure example */
+ typedef struct
+ {
+    /* declare the User Module Context "local" data here */
+
+ } Receiver_user_context;
+
+
+
+ #if defined(__cplusplus)
+ } 
+ #endif /* __cplusplus */
+
+ #endif  /* Receiver_USER_CONTEXT_H */
---
+++ Examples/simple_event_example/01-Components/Sender/C/inc/Sender_user_context.h
@@ -0,0 +1,30 @@
+/* @file Sender_user_context.h
+ * This is an example of a user defined User Module context
+ */
+
+ #if !defined(Sender_USER_CONTEXT_H)
+ #define Sender_USER_CONTEXT_H
+
+ #if defined(__cplusplus)
+ extern "C" {  
+ #endif /* __cplusplus */
+
+ #include "extra_types.h"
+ #include "extra_types_types.h"
+ /* Container Types */
+ #include "Sender_container_types.h"
+
+ /* User Module Context structure example */
+ typedef struct
+ {
+    /* declare the User Module Context "local" data here */
+
+ } Sender_user_context;
+
+
+
+ #if defined(__cplusplus)
+ } 
+ #endif /* __cplusplus */
+
+ #endif  /* Sender_USER_CONTEXT_H */
