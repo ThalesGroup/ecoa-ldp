674f1c7817ffabb07cc43c018cd05fe23184b744
Gerhard Dutka <gerhard.dutka@hensoldt.net>
2026-02-20 10:31:01 +0100

    chore: updated build script

--- containers/build_container.sh
+++ containers/build_container.sh
@@ -6,14 +6,14 @@ export TOP
 # <dockerfile> <image label> <docker directory>
 
 function build_container() {
-	echo "- building"
-	cat "$2" \
-		| docker buildx build \
-		--progress=plain \
-		--build-arg HTTP_PROXY=$http_proxy \
-		--build-arg HTTPS_PROXY=$https_proxy \
-    --build-arg NO_PROXY=$no_proxy \
-		-t "$1" -f - .
+  echo "- building"
+  cat "$2" \
+  | docker -l info buildx build \
+  --progress=plain --debug --no-cache \
+  --build-arg HTTP_PROXY=$http_proxy \
+  --build-arg HTTPS_PROXY=$https_proxy \
+  --build-arg NO_PROXY=$no_proxy \
+  -t "$1" -f - .
 }
 
-build_container ecoa-as7-zenoh-devel Dockerfile
+build_container tmp_ecoa-as7-zenoh-devel Dockerfile
